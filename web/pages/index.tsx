01aa7a254843a025e46994cc2d6973738ab54e8c
Justin <justintorre75@gmail.com>
2023-07-26 21:20:31 -0700

    .

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -29,8 +29,11 @@ export default Home;
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
+  console.log("CONTEXT", context);
   const client = createPagesServerClient(context, {
     supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE",
   });
   console.log(
     "env",
